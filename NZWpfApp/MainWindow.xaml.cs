3a5540488a8c9c9e3ce6de6b03571bd444fc6d25
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-02-12 01:35:39 +0100

    l'ajout des regions 100%

--- NZWpfApp/MainWindow.xaml.cs
+++ NZWpfApp/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using Newtonsoft.Json;
+using NZWpfApp.Models.Domain;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NZWpfApp
 {
@@ -16,9 +12,60 @@ namespace NZWpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly HttpClient client = new HttpClient();
         public MainWindow()
         {
+            client.BaseAddress = new Uri("https://localhost:7283/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
             InitializeComponent();
+            this.getAll();
+
+
+        }
+
+
+
+        public async void getAll()
+        {
+            
+            var response = await client.GetStringAsync("Regions");
+            var regions = JsonConvert.DeserializeObject<List<Models.Domain.Region>>(response);
+            Console.WriteLine(regions);
+            regionDG.ItemsSource = regions;
+            labl.Content = "random"; 
+
+        }
+
+        private async void AddRegion(object sender, RoutedEventArgs e)
+        {
+            var newRegion = new Region
+            {
+                Name = nameRn.Text ,
+                Code= codeRn.Text ,
+                ReigionImageUrl= regionimgurlRn.Text
+            };
+            var json = JsonConvert.SerializeObject(newRegion);
+            var response = await client.PostAsync("Regions", new StringContent(json, Encoding.UTF8, "application/json"));
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Région ajoutée avec succès!");
+                // Réinitialiser les données du formulaire
+                var NewRegion = new Region();
+                
+                // Recharger la liste des régions après l'ajout
+                getAll();
+            }
+            else
+            {
+                MessageBox.Show("Erreur lors de l'ajout de la région.");
+            }
+
+
+            
+
         }
     }
 }
